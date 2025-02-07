15a32442ebab902511f670aad4699b12fe3b7dd1
DungTQ <tranquocdung11052000@gmail.com>
2025-02-07 23:02:29 +0700

    fix csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'api/login',
-        'api/logout',
-        'api/register'
+        'login',
+        'logout',
+        'register'
     ];
 }
