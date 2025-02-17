89490e4102ef02eeb347c62dfb45fdca20e5a740
DungTQ <tranquocdung11052000@gmail.com>
2025-02-17 16:47:13 +0700

    response base

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -9,21 +10,33 @@ class BaseController extends Controller
     {
         $limit = $request->input('limit', 10);
         $data = $query->paginate($limit);
-    
+
         // Thêm URL đầy đủ vào `image_path` và `file_path`
         $data->getCollection()->transform(function ($product) {
             $product->image_path = $product->image_path ? env('URL_IMAGE') . $product->image_path : null;
             $product->file_path = $product->file_path ? env('URL_IMAGE') . $product->file_path : null;
             return $product;
         });
-    
+
         return response()->json([
             'r' => 0,
             'msg' => $message,
             'data' => $data->items(),
+            'meta' => [
+                'current_page' => $data->currentPage(),
+                'last_page' => $data->lastPage(),
+                'per_page' => $data->perPage(),
+                'total' => $data->total(),
+            ],
+            'links' => [
+                'first' => $data->url(1),
+                'last' => $data->url($data->lastPage()),
+                'prev' => $data->previousPageUrl(),
+                'next' => $data->nextPageUrl(),
+            ]
         ]);
     }
-    
+
 
     public function successResponse($data, $message = "Success")
     {
@@ -43,4 +56,3 @@ class BaseController extends Controller
         ], $code);
     }
 }
-
