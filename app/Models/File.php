c54f869199ad99869f7841a63b085c0f56829b44
HoangLV <hoanglv@stringee.com>
2025-03-01 13:28:08 +0700

    Update

--- app/Models/File.php
+++ app/Models/File.php
@@ -1,19 +1,27 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin  Builder
+ */
 class File extends Model
 {
     use HasFactory;
 
     protected $fillable = ['id', 'file_name', 'file_path'];
 
+    public static string $MODEL_FILE_PATH = '/models';
+    public static string $IMAGE_FILE_PATH = '/images';
+
     public function product()
     {
         return $this->belongsTo(Product::class);
     }
+
     public function files()
     {
         return $this->hasMany(ProductFiles::class);
