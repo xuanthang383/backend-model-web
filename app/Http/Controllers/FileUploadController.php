e04be130883a20a69d84551040ef60790f3c8554
DungTQ <tranquocdung11052000@gmail.com>
2025-04-28 09:48:13 +0700

    save avatar name by user_id

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -59,48 +59,6 @@ class FileUploadController extends BaseController
     /**
      * Hàm chung xử lý upload file
      */
-    private function uploadFile(Request $request, $folder, $maxSize, $successMessage)
-    {
-        try {
-            // Validate file
-            $request->validate([
-                'file' => "required|file|max:$maxSize"
-            ]);
-
-            $file = $request->file('file');
-
-            // Kiểm tra file có hợp lệ không
-            if (!$file->isValid()) {
-                Log::error("Upload thất bại: " . $file->getErrorMessage(), [
-                    'filename' => $file->getClientOriginalName(),
-                    'folder' => $folder
-                ]);
-                return response()->json(['error' => 'File upload failed', 'message' => $file->getErrorMessage()], 400);
-            }
-
-            // Lưu file tạm thời
-            $fileUrl = $this->storeTempFile($file, $folder);
-
-            if (!$fileUrl) {
-                return response()->json(['error' => 'File upload failed'], 500);
-            }
-
-            return response()->json([
-                'message' => $successMessage,
-                'file_url' => $fileUrl
-            ]);
-        } catch (\Illuminate\Validation\ValidationException $e) {
-            Log::error("Validation lỗi khi upload: " . $e->getMessage(), [
-                'folder' => $folder
-            ]);
-            return response()->json(['error' => 'File upload failed', 'message' => $e->getMessage()], 400);
-        } catch (\Exception $e) {
-            Log::error("Lỗi không xác định khi upload: " . $e->getMessage(), [
-                'folder' => $folder
-            ]);
-            return response()->json(['error' => 'Server error'], 500);
-        }
-    }
     public function uploadAvatar(Request $request)
     {
         try {
@@ -111,12 +69,12 @@ class FileUploadController extends BaseController
 
             $fileUrl = $request->input('file_url');
             $originalFileName = basename($fileUrl);
-            $timestamp = time();
-            $fileName = $timestamp . "_" . $originalFileName;
+            $extension = pathinfo($originalFileName, PATHINFO_EXTENSION);
+            $userId = (int)$this->getUserIdFromToken($request);
+            $fileName = "{$userId}.{$extension}";
 
-            // Lưu URL avatar cũ
+            // Lưu URL avatar
             $user = Auth::user();
-            $oldAvatarUrl = $user->avatar;
 
             // Đẩy lên queue để upload lên S3
             dispatch(new UploadFileToS3(null, $fileUrl, 'images'));
@@ -128,15 +86,6 @@ class FileUploadController extends BaseController
             $user->avatar = $s3Url;
             $user->save();
 
-            // Xóa avatar cũ trên S3 nếu tồn tại
-            if ($oldAvatarUrl) {
-                $oldFilePath = parse_url($oldAvatarUrl, PHP_URL_PATH);
-                $oldFileName = basename($oldFilePath);
-                $oldS3Path = "images/{$oldFileName}";
-
-                Storage::disk('s3')->delete($oldS3Path);
-            }
-
             return response()->json([
                 'message' => 'Avatar uploaded successfully',
                 'avatar_url' => $s3Url // Trả về URL từ S3
@@ -145,8 +94,4 @@ class FileUploadController extends BaseController
             return response()->json(['error' => 'Server error', 'message' => $e->getMessage()], 500);
         }
     }
-
-
-
-
 }
